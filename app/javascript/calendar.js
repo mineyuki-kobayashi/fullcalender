f37bf91088d5c5bf573aa2919456907e609a8566
mineyuki-kobayashi <mine_e89@yahoo.co.jp>
2020-10-11 14:25:08 +0900

    カレンダー実装

---
+++ app/javascript/calendar.js
@@ -0,0 +1,17 @@
+import { Calendar } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
+import jaLocale from '@fullcalendar/core/locales/ja';
+
+document.addEventListener('turbolinks:load', function() {
+  var calendarEl = document.getElementById('calendar');
+
+  var calendar = new Calendar(calendarEl, {
+    plugins: [ dayGridPlugin, interactionPlugin ],
+    locale: jaLocale,
+    dateClick: function(info){
+      alert('Clicked on: ' + info.dateStr);
+    }
+  });
+  calendar.render();
+});
